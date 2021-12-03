33564d0d4ee1d3955edcd29bbd7dd144ea57e89a
Gaurav Bhat <42300572+Grbmax@users.noreply.github.com>
2021-12-03 23:42:49 +0530

    03-12-2021

--- migrate/tedx.sql
+++ migrate/tedx.sql
@@ -6,10 +6,19 @@ CREATE TABLE `tedx`.`newsletter` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
-  `ph_no` INT NOT NULL,
+  `organisation` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`));
 
-CREATE TABLE `tedx`.`reg` (
+CREATE TABLE `tedx`.`event_reg` (
+    `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `f_name` varchar(45) NOT NULL,
+    `l_name` varchar(45) NOT NULL,
+    `addr`   varchar(45) NOT NULL,
+    `email`  varchar(45) NOT NULL,
+    `ph_no`  INT(12) NOT NULL
+);
+
+CREATE TABLE `tedx`.`pre_event_reg` (
     `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `f_name` varchar(45) NOT NULL,
     `l_name` varchar(45) NOT NULL,
