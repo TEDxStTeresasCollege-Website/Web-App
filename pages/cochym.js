de0edb4c8f13a82916e60c3d09baef75bc455eb4
sharunrajeev <sharunrajeev25@gmail.com>
2021-12-04 22:52:08 +0530

    Fix Cochym Homepage

--- pages/cochym.js
+++ pages/cochym.js
@@ -14,14 +14,14 @@ const Cochym = () => {
 
             <Navbar />
 
-            <div className="min-h-screen bg-night-sky bg-repeat-y bg-black text-white pt-56 flex flex-col justify-center items-center">
+            <div className="min-h-screen bg-night-sky bg-repeat-y bg-black text-white pt-40 flex flex-col justify-center items-center">
                 <h1 className="text-center text-2xl lg:text-4xl text-ted-red serif block mb-20">Pre-Event Registration Form</h1>
                 <div className="">
                     <Image className="" src="/cochymLogo.png" width="700" height="250" layout="intrinsic"></Image>
                 </div>
 
                 <div className="w-1/2 mx-auto mt-20">
-                    <form className="">
+                    <form className="bg-gray-800 p-6 rounded-lg m-2 bg-opacity-60">
                         <div className="grid grid-cols-2 gap-5 justify-center">
                             <label className="text-xl md:text-2xl">First Name :</label>
                             <input className="rounded-sm"></input>
