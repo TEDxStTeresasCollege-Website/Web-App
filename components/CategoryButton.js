456f3d7cd62fe42892c6c2e781fed4a982983e47
Sharun <sharunpublic@gmail.com>
2021-12-06 20:05:26 +0530

    Complete Team page prototype

--- components/CategoryButton.js
+++ components/CategoryButton.js
@@ -1,6 +1,6 @@
-const Button = ({text}) => {
+const Button = ({text, click}) => {
     return (
-        <button className="text-white bg-water hover:border hover:border-solid hover:border-water hover:bg-white hover:text-water active:bg-blue-600 font-bold uppercase text-xs px-6 py-3 rounded-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">{text}</button>
+        <button className="text-white bg-water hover:border hover:border-solid hover:border-water hover:bg-white hover:text-water active:bg-blue-600 font-bold uppercase text-xs px-6 py-3 rounded-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" onClick={click} type="button">{text}</button>
     );
 }
 
