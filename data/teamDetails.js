e28f6e2d864c626c95c90169f4ad429d6e014a50
Ankitha <ankithatonypp@gmail.com>
2021-12-08 14:26:43 +0530

    core team details and images

--- data/teamDetails.js
+++ data/teamDetails.js
@@ -3,9 +3,9 @@ const TeamDetails = [
         content: "Team work undoubtedly makes the dream work and we have no better example of this than our own crew of volunteers! During this year, 95 people decided to come together under the common banner of TEDxStTeresasCollege to spread ideas they believe are worth sharing. Today, these 95 people are volunteers who are proud and ready to host their very first TEDx event!",
         image: "/team.jpg"
      }, {
-         name : ["Sneha Ann George", "Marianne Joseph"],
-         designation: ["Organizer", "Co-organizer"],
-         image: ["/coreTeam/sneha.jpg", "/coreTeam/marianne.jpg"],
+         name : ["Sneha Ann George", "Marianne Joseph","Christeena Abraham","Anna Kattampally","Aisha Hanan","Sreelakshmi Jayaprakash Pillai","Diya Mary Joshy","Nesrin KS","Boon Mourin Sebastine","Kate Roshan"],
+         designation: ["Organizer", "Co-organizer","Curator","Event Manager","Design and Media Director","Marketing Director","Budgeting Manager","Website Manager","Operations Crew Head","Sponsorship Manager"],
+         image: ["/coreTeam/sneha.png", "/coreTeam/marianne.png","/coreTeam/christeena.png","/coreTeam/anna.png","/coreTeam/aisha.png","/coreTeam/sree.png","/coreTeam/diya.png","/coreTeam/nesrin.png","/coreTeam/boon.png","/coreTeam/kate.png"],
       }, {
           content: "It's the era of social media and quality content is a game-changer. With pens stuck behind their ears and imaginations running wild,we present to you the curious minds who work to change your perspective towards ideas you never gave a second thought about. Here's our very own Content Curation Team!",
           image: "/content.jpg"
