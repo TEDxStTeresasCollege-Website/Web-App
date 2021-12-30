3e248c4fbebb6036f2ad408e0d1d0c5ba7cffbb0
sharun <sharunpublic@gmail.com>
2021-12-30 12:46:00 +0530

    Update Cynosure web version

--- pages/cynosure.js
+++ pages/cynosure.js
@@ -24,46 +24,47 @@ const cynosure = () => {
             <Navbar />
 
             {/* The main section */}
-            <div className="text-center bg-white-dots">
-                <div className="pt-32 pb-10">
+            <div className="text-center bg-white-dots md:w-4/5 mx-auto">
+                <div className="pt-32 pb-10 md:w-4/5 md:pb-6 lg:w-11/12 mx-auto">
                     <Image className="" src="/CynosureLogo.png" height={2251} width={2251} />
                 </div>
                 <div className="px-10">
-                    <p className="text-lg">Non aute anim exercitation aute nostrud ex ad do in dolor non velit. Occaecat ex excepteur exercitation aliquip ut nostrud enim consectetur.</p>
-                    <p className="text-lg">Non aute anim exercitation aute nostrud ex ad do in dolor non velit. Occaecat ex excepteur exercitation aliquip ut nostrud enim consectetur.</p>
+                    <p className="text-lg lg:text-2xl xl:text-3xl">Non aute anim exercitation aute nostrud ex ad do in dolor non velit. Occaecat ex excepteur exercitation aliquip ut nostrud enim consectetur.</p>
+                    <p className="text-lg lg:text-2xl xl:text-3xl pt-5">Non aute anim exercitation aute nostrud ex ad do in dolor non velit. Occaecat ex excepteur exercitation aliquip ut nostrud enim consectetur.</p>
                 </div>
             </div>
 
             {/* Speakers section */}
-            <div className="px-5 bg-white-dots">
-                <h1 className="serif text-4xl my-20 font-bold text-center hover:underline">Unveiling the Voices</h1>
-                <div className="grid grid-cols-1">
-                    <div className="mx-auto pb-5">
-                        <Image src="/cynosure/Meera.png" width="971" height="971"></Image>
-                        <h2 className="serif text-3xl font-semibold text-center pt-5">Meera Singania</h2>
+            <div className="px-5 bg-white-dots md:w-4/5 lg:w-5/6 mx-auto">
+                <h1 className="serif text-4xl md:text-5xl lg:text-6xl my-20 font-bold text-center hover:underline">Unveiling the Voices</h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 md:items-center">
+                    <div className="mx-auto p-5 md:p-0 md:pr-5 md:w-full">
+                        <img className="" src="/cynosure/Meera.png" width="971" height="971"></img>
                     </div>
                     <div className="text-justify">
-                        <p>Ea et in eiusmod consequat adipisicing minim cupidatat pariatur. Consectetur consequat elit aute amet incididunt eu laboris ut incididunt cillum enim aliqua magna mollit. Labore incididunt cillum irure sint enim incididunt nisi mollit ipsum. Elit culpa eu irure nostrud et esse enim exercitation dolore Lorem proident nisi. Tempor commodo voluptate sunt veniam in ex ea est aliquip. Nisi velit voluptate ea minim nisi mollit enim in voluptate deserunt aliquip sint incididunt.</p>
+                        <h2 className="serif text-3xl md:text-2xl lg:text-4xl font-semibold text-center py-5 md:pt-0">Meera Singania</h2>
+                        <p className="md:text-sm lg:text-xl">Ea et in eiusmod consequat adipisicing minim cupidatat pariatur. Consectetur consequat elit aute amet incididunt eu laboris ut incididunt cillum enim aliqua magna mollit. Labore incididunt cillum irure sint enim incididunt nisi mollit ipsum. Elit culpa eu irure nostrud et esse enim exercitation dolore Lorem proident nisi. Tempor commodo voluptate sunt veniam in ex ea est aliquip. Nisi velit voluptate ea minim nisi mollit enim in voluptate deserunt aliquip sint incididunt.</p>
                     </div>
-                    <div className="mx-auto pb-5 pt-20">
-                        <Image src="/cynosure/Meera.png" width="971" height="971"></Image>
-                        <h2 className="serif text-3xl font-semibold text-center pt-5">Meera Singania</h2>
-                    </div>
+                </div>
+                <div className="mt-20 grid grid-cols-1 md:grid-cols-2 md:items-center">
                     <div className="text-justify">
-                        <p>Ea et in eiusmod consequat adipisicing minim cupidatat pariatur. Consectetur consequat elit aute amet incididunt eu laboris ut incididunt cillum enim aliqua magna mollit. Labore incididunt cillum irure sint enim incididunt nisi mollit ipsum. Elit culpa eu irure nostrud et esse enim exercitation dolore Lorem proident nisi. Tempor commodo voluptate sunt veniam in ex ea est aliquip. Nisi velit voluptate ea minim nisi mollit enim in voluptate deserunt aliquip sint incididunt.</p>
+                        <h2 className="serif text-3xl md:text-2xl lg:text-4xl font-semibold text-center py-5 md:pt-0">Meera Singania</h2>
+                        <p className="md:text-sm lg:text-xl">Ea et in eiusmod consequat adipisicing minim cupidatat pariatur. Consectetur consequat elit aute amet incididunt eu laboris ut incididunt cillum enim aliqua magna mollit. Labore incididunt cillum irure sint enim incididunt nisi mollit ipsum. Elit culpa eu irure nostrud et esse enim exercitation dolore Lorem proident nisi. Tempor commodo voluptate sunt veniam in ex ea est aliquip. Nisi velit voluptate ea minim nisi mollit enim in voluptate deserunt aliquip sint incididunt.</p>
+                    </div>
+                    <div className="mx-auto p-5 md:p-0 md:pl-5 md:w-full">
+                        <img className="" src="/cynosure/Meera.png" width="971" height="971"></img>
                     </div>
                 </div>
             </div>
 
             {/* Performer LineUp */}
-            <div className="px-5 bg-white-dots">
-                <h1 className="serif text-4xl mt-20 pb-5 font-bold text-center hover:underline">Performers Line Up</h1>
+            <div className="px-5 bg-white-dots w-full md:w-4/5 lg:w-5/6 mx-auto">
+                <h1 className="serif text-4xl md:text-5xl lg:text-6xl my-20 font-bold text-center hover:underline">Performers Line Up</h1>
                 <div className="h-40 w-full bg-purple-900">
-
                 </div>
                 <div className="py-5 flex justify-center">
                     <Link href='/register'>
-                        <button className="bg-purple-500 hover:bg-purple-700 text-white py-2 px-4 rounded text-sm">
+                        <button className="bg-purple-500 hover:bg-purple-700 text-white py-2 px-4 rounded text-sm lg:text-lg">
                             Register Now
                         </button>
                     </Link>
@@ -71,9 +72,9 @@ const cynosure = () => {
             </div>
 
             {/* Footer section */}
-            <footer>
-                <div className="w-full mt-20 text-sm">
-                    <div className=""><img src="/mobile/FooterGirl.png" width="1081" height="1453" /></div>
+            <footer className="">
+                <div className="w-full mt-20 text-sm lg:text-lg xl:text-xl">
+                    <div className="bg-white-dots"><img src="/mobile/FooterGirl.png" width="1081" height="1453" /></div>
                     <div className="p-5 w-full bg-black">
                         <div className="flex flex-row">
                             <div className="text-left flex-1">
