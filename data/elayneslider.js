ebc843e038ee4793a7ed2cdb79efa2b75829ba88
Neha <nehaneenus333@gmail.com>
2022-08-23 14:00:59 +0530

    elayneslider,upnext,navbar

---
+++ data/elayneslider.js
@@ -0,0 +1,10 @@
+const elayneslider = [
+    "/elayne/ROHIT NAIR AND NANDAGOPAN.png",
+    "/elayne/myself and my moves (1).png",
+    "/elayne/REMA JEEVAN .png",
+    "/elayne/RJ MIKE .png",
+    "/elayne/stc fashion team .png",
+    "/elayne/FIREN.png",
+];
+
+export default elayneslider
